f70db73eaca91511f718178ae75a825f5a988495
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-21 23:58:12 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,7 +1,22 @@
 module Memorable
-  def reset_all 
-  end 
   
-  def count 
-  end 
+  module ClassMethods
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+  end
+
 end
