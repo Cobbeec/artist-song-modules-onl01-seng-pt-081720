fcdd1f99dc5854779170e2ae497e9b062e99ec44
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-22 00:37:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,63 +1,63 @@
-require 'pry'
+# require 'pry'
 
-class Artist
-   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend Findable
-  include Paramable 
-  # extend Memorable 
-  attr_accessor :name
-  attr_reader :songs
+# class Artist
+#   extend Memorable::ClassMethods
+#   include Memorable::InstanceMethods
+#   extend Findable
+#   include Paramable 
+#   # extend Memorable 
+#   attr_accessor :name
+#   attr_reader :songs
 
-  @@artists = []
+#   @@artists = []
 
-  def initialize
-    # self.class.all << self
-     super
-    @songs = []
-end
+#   def initialize
+#     # self.class.all << self
+#     super
+#     @songs = []
+# end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
+#   # def self.find_by_name(name)
+#   #   @@artists.detect{|a| a.name == name}
+#   # end
 
-  def self.all
-    @@artists
-  end
+#   def self.all
+#     @@artists
+#   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
+#   # def self.reset_all
+#   #   self.all.clear
+#   # end
 
-  # def self.count
-  #   self.all.count
-  # end
+#   # def self.count
+#   #   self.all.count
+#   # end
+
+# #   def add_song(song)
+# #     @songs << song
+# #     song.artist = self
+# #   end
+
+# #   # def add_songs(songs)
+# #   #   songs.each { |song| add_song(song) }
+# #   # end
+
+# # def add_song(song)
+# #     @songs << song
+# #     song.artist = self
+# #     songs.each { |song| add_song(song) }
+# #   end
 
 #   def add_song(song)
 #     @songs << song
-#     song.artist = self
+#     song.artist = self unless song.artist
 #   end
 
-#   # def add_songs(songs)
-#   #   songs.each { |song| add_song(song) }
-#   # end
-
-# def add_song(song)
-#     @songs << song
-#     song.artist = self
+#   def add_songs(songs)
 #     songs.each { |song| add_song(song) }
 #   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self unless song.artist
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
