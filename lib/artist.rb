5ed4cfe6feaafaa61d2df8ebe03892f09b9eb83e
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-22 00:27:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,20 +33,20 @@ end
   #   self.all.count
   # end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
 
-  # def add_songs(songs)
-  #   songs.each { |song| add_song(song) }
-  # end
+#   # def add_songs(songs)
+#   #   songs.each { |song| add_song(song) }
+#   # end
 
-def add_song(song)
-    @songs << song
-    song.artist = self
-    songs.each { |song| add_song(song) }
-  end
+# def add_song(song)
+#     @songs << song
+#     song.artist = self
+#     songs.each { |song| add_song(song) }
+#   end
 
   def to_param
     name.downcase.gsub(' ', '-')
